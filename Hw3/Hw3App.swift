a5e7bb596fd097cca30e054c39d30b128c8a1f50
Wilhelm Shen <33245687+WilhelmIllust@users.noreply.github.com>
2020-11-01 20:18:44 +0800

    Initial Commit

---
+++ Hw3/Hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  Hw3App.swift
+//  Hw3
+//
+//  Created by User11 on 2020/11/1.
+//
+
+import SwiftUI
+
+@main
+struct Hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
